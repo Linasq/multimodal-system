4a41bbb5e5f5028e0d3143b12e1dda21ec2f306e
Linas <linasmg58@gmail.com>
2024-12-06 08:48:04 +0100

    add function, that verifies user

--- face.py
+++ face.py
@@ -1,14 +1,23 @@
 from deepface import DeepFace
 
-result = DeepFace.verify(img1_path='db/admin/img3.png', img2_path='db/bill.png')#, model_name='Facenet512')
-print(result)
 
-#TODO
+def verify_face(login: str, img1_path: str) -> bool:
+    # there we can find registered user's photo 
+    try:
+        img2_path = f'db/{login}/img.png'
+        result = DeepFace.verify(img1_path=img1_path, img2_path=img2_path)
+        return result['result']
+    except:
+        print('User has not registered yet')
 
-'''
-create function that returns:
-    - true if person matches
-    - false if not
+    return False
 
-create function that saves images of registered users
-'''
+
+#TODO check how to send file through function or register user in main function
+def register_user_face(login: str):
+    pass
+
+
+if __name__ == '__main__':
+    result = DeepFace.verify(img1_path='db/admin/img1.png', img2_path='db/admin/img2.png')
+
