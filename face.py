1cc2b251a7bce57a9bb4812dd347c26ce98774da
Linas <linasmg58@gmail.com>
2024-12-09 17:53:44 +0100

    add requirement for resemblyzer, face.py - final version

--- face.py
+++ face.py
@@ -1,11 +1,11 @@
 from deepface import DeepFace
 
 
-def verify_face(login: str, img1_path: str) -> bool:
+def verify_face(login: str, img_path: str) -> bool:
     # there we can find registered user's photo 
     try:
         img2_path = f'db/{login}/img.png'
-        result = DeepFace.verify(img1_path=img1_path, img2_path=img2_path)
+        result = DeepFace.verify(img1_path=img_path, img2_path=img2_path)
         return result['result']
     except:
         print('User has not registered yet')
@@ -13,11 +13,6 @@ def verify_face(login: str, img1_path: str) -> bool:
     return False
 
 
-#TODO check how to send file through function or register user in main function
-def register_user_face(login: str):
-    pass
-
-
 if __name__ == '__main__':
     result = DeepFace.verify(img1_path='db/admin/img1.png', img2_path='db/admin/img2.png')
-
+    
