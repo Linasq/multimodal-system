dba8fa36e0a91566be86bd0e8a2b577a7ccd4b0f
Linas <linasmg58@gmail.com>
2025-01-03 13:05:06 +0100

    upiekszono back_end, testowanie face.py w celu naprawienia login

--- face.py
+++ face.py
@@ -1,5 +1,8 @@
 from deepface import DeepFace
 import logging
+from sklearn.metrics.pairwise import cosine_distances
+import json
+import numpy as np
 
  # set level of logging
 logger = logging.getLogger()
@@ -14,23 +17,63 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+def save_embedding(login: str, is_login: bool):
+    if is_login:
+        img_path = f'tmp/{login}/img.png'
+    else:
+        img_path = f'db/{login}/img.png'
+
+    result = DeepFace.represent(img_path=img_path)
+    logger.info(f'Created embedding of user {login}')
+    return result
+
+
 def verify_face(login: str):
-   # there we can find registered user's photo 
-    # try:
+    try:
         img2_path = f'db/{login}/img.png'
         img1_path = f'tmp/{login}/img.png'
 
-        result = DeepFace.verify(img1_path=img1_path, img2_path=img2_path)
-        logger.info(f'Successfully added photo of user {login}')
-        return result['result']
-    # except:
+        # result = cosine_similarity()
+        logger.info(f'Successfully calculated cosine similarity of user {login}')
+        # return result['result']
+    except:
         logger.error('User has not been registered yet')
 
-    # return False
+    return False
 
 
 if __name__ == '__main__':
-    result = DeepFace.verify(img1_path='db/Linas/img.png', img2_path='tmp/Linas/img.png')
-    logger.info('Successfully verified user')
-    print(result)
+    emb1 = DeepFace.represent('db/Linas/img.png')
+    emb2 = DeepFace.represent('tmp/Linas/img.png')
+
+    with open('db/Linas/test_emb.txt', 'w') as f:
+        f.write(str(emb1[0]))
+
+    with open('tmp/Linas/test_emb.txt', 'w') as f:
+        f.write(str(emb2[0]))
+
+    with open('db/Linas/test_emb.txt', 'r') as f:
+        test1 = f.read()
+        test1 = test1.replace("'", '"')
+        test1 = test1.replace('None', '"None"')
+
+    with open('tmp/Linas/test_emb.txt', 'r') as f:
+        test2 = f.read()
+        test2 = test2.replace("'", '"')
+        test2 = test2.replace('None', '"None"')
+
+    test1 = json.loads(test1)
+    test1 = test1['embedding']
+    test1 = np.array(test1)
+
+    test2 = json.loads(test2)
+    test2 = test2['embedding']
+    test2 = np.array(test2)
+
+    dot_product = np.dot(test1, test2)
+    source_norm = np.linalg.norm(test1)
+    test_norm = np.linalg.norm(test2)
+    distances = 1 - dot_product / (source_norm * test_norm)
+
+    print(distances)
     
